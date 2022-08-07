4e52d96a6efd7b87c058e1d2fe5f4e93f3669649
esk98 <eesk98@gmail.com>
2022-08-07 20:58:22 +0700

    added 2 pages

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React from 'react'
-import {
-  TransitionGroup,
-  CSSTransition
-} from "react-transition-group";
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout  from './components/Layout/Layout'
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import './app.scss';
 function App() {
-  let location = useLocation();
   return (
-    <TransitionGroup>
-      <CSSTransition
-        ke={location.pathname}
-        classNames="fade"
-        timeout={300}
-      >
-        <Switch location={location}>
-          <Routes>
-            <Route path="/" element={<Layout />} >
-              <Route index element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="projects" element={<Projects />} />
-            </Route>
-          </Routes>
-        </Switch>
-      </CSSTransition>
-    </TransitionGroup>
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+      </Route>
+    </Routes>
   );
 }
 
