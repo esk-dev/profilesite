64b5964e8e23e8eebced5158d8aa2e77d68925f4
esk98 <eesk98@gmail.com>
2022-08-07 12:44:39 +0700

    Make text area animation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,19 +17,21 @@ const Home = () => {
       <div className="text-zone">
         <h1>
           <span className={letterClass}>H</span>
-          <span className={`${letterClass} _2`}>i,</span>
+          <span className={`${letterClass} _2`}>i</span>
+          <span className={`${letterClass} _3`}>,</span>
           <br/>
-          <span className={`${letterClass} _3`}>I</span>
-          <span className={`${letterClass} _4`}>'m </span>
+          <span className={`${letterClass} _4`}>I</span>
+          <span className={`${letterClass} _5`}>'</span>
+          <span className={`${letterClass} _6`}>m </span>
         <AnimatedLetters
         letterClass={letterClass}
         strArray={' Egor'}
-        idx={6}/>
+        idx={7}/>
         <br/>
         <AnimatedLetters
         letterClass={letterClass}
         strArray={'Junior Frontend Developer'}
-        idx={15}
+        idx={12}
         />
         </h1>
         <h2> Java Script / Angular</h2>
