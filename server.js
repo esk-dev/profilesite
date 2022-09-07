316163e8acfa399dc61621078bf6c5e5688f8511
Egor <eesk98@gmail.com>
2022-09-07 19:39:11 +0700

    Fixing deploy: change manifest & icon/ change serv

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/public/profilesite'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/public/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Start the app by listening on the default Heroku port
